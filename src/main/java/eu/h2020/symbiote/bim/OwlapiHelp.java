cf8d6555d0bb074c1f494f322e2f914db5b44d83
luca- <luca.tommy.io@gmail.com>
2017-06-26 18:49:31 +0200

    add type to parameter

--- src/main/java/eu/h2020/symbiote/bim/OwlapiHelp.java
+++ src/main/java/eu/h2020/symbiote/bim/OwlapiHelp.java
@@ -9,11 +9,15 @@ package eu.h2020.symbiote.bim;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import static org.apache.jena.assembler.Assembler.reasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.formats.ManchesterSyntaxDocumentFormat;
 import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
@@ -31,6 +35,7 @@ import org.semanticweb.owlapi.util.*;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.semanticweb.owlapi.vocab.OWLFacet;
 import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+import uk.ac.manchester.cs.owl.owlapi.OWLObjectPropertyImpl;
 
 /**
  *
@@ -42,6 +47,7 @@ public class OwlapiHelp {
 
     private static final String CIM_FILE = "/core-v0.6.owl";
     private static final String BIM_FILE = "/bim-0.3.owl";
+    
 
     public HashMap<String,HashMap<String,ArrayList<String>>> test() {
         HashMap<String,HashMap<String,ArrayList<String>>> map = new HashMap<String,HashMap<String,ArrayList<String>>>();
@@ -50,9 +56,13 @@ public class OwlapiHelp {
         File file = new File(name);
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         OWLOntology ontology;
+        OWLDataFactory dataFactory;
+        OWLReasoner reasoner;        
 
         try {
             ontology = manager.loadOntologyFromOntologyDocument(file);
+            //reasoner = reasonerFactory.createReasoner(ontology);
+            dataFactory = manager.getOWLDataFactory();
 
             Set<OWLClass> classes;
             Set<OWLObjectProperty> prop;
@@ -66,6 +76,101 @@ public class OwlapiHelp {
             //configurator = new OWLAPIOntologyConfigurator(this);    
         
 
+           
+            
+            
+
+            for (OWLClass c : classes) {
+                OWLDataProperty owlDataProperty = dataFactory.getOWLDataProperty(c.getIRI());
+                //System.out.println(owlDataProperty.getIRI());
+                
+                OWLObjectProperty owlObjectProperty = dataFactory.getOWLObjectProperty(c.getIRI());
+                //System.out.println(owlObjectProperty.getIRI());
+                
+                OWLDatatype owlDatatype = dataFactory.getOWLDatatype(c.getIRI());
+                System.out.println(owlDatatype.getIRI());
+                
+                String breakPP = "";
+                if(owlDatatype.getIRI().toString().equals("http://www.symbiote-h2020.eu/ontology/core#InputParameter"))
+                    breakPP = "TRUE";
+                
+                
+                System.out.println("\t +PROPRIETA': " );
+                for (OWLSubClassOfAxiom axiom : ontology.getSubClassAxiomsForSubClass(c))
+                {
+                        Set<OWLClass> associatedTerms = new HashSet<OWLClass>();
+                        OWLClassExpression expression = axiom.getSuperClass();
+                        
+                        if (expression.isAnonymous())
+                        {
+                            String typeClass ="";
+                            String namePro="";
+                                    
+                            Stream<OWLDataProperty> owlDataPropertyStream = expression.dataPropertiesInSignature();
+                            Optional<OWLDataProperty> owlDataPropertyOptional = owlDataPropertyStream.findFirst();
+                            if(owlDataPropertyOptional.isPresent())
+                                namePro = owlDataPropertyOptional.get().getIRI().getShortForm();      
+                            
+                            Stream<OWLObjectProperty> owlObjectPropertyStream = expression.objectPropertiesInSignature();
+                            Optional<OWLObjectProperty> owlObjectPropertyOptional = owlObjectPropertyStream.findFirst();
+                            if(owlObjectPropertyOptional.isPresent())
+                                namePro = owlObjectPropertyOptional.get().getIRI().getShortForm();                                  
+                                    
+                            
+                            
+                            Stream<OWLDatatype> owlDatatypeStream = expression.datatypesInSignature();
+                            Optional<OWLDatatype> owlDatatypeOptional = owlDatatypeStream.findFirst();
+                            if(owlDatatypeOptional.isPresent())
+                                typeClass = owlDatatypeOptional.get().getIRI().getShortForm();
+                            
+                            Stream<OWLClass> owlClassStream = expression.classesInSignature();
+                            Optional<OWLClass> owlClassOptional = owlClassStream.findFirst();
+                            if(owlClassOptional.isPresent())
+                                typeClass = owlClassOptional.get().getIRI().getShortForm();
+                            
+                            
+                            System.out.println("\t\t +: " + typeClass + " " + namePro);
+                        }
+                }
+                
+                Iterator<OWLSubClassOfAxiom> axiomIterator = ontology.subClassAxiomsForSuperClass(c).iterator();
+                while (axiomIterator.hasNext())
+                {
+                    OWLSubClassOfAxiom axiom = axiomIterator.next();
+                        Set<OWLClass> associatedTerms = new HashSet<OWLClass>();
+                        OWLClassExpression expression = axiom.getSuperClass();
+                        System.out.println("\t +PROPRIETA': " );
+                        if (expression.isAnonymous())
+                        {
+                            String type ="";
+                            String namePro="";
+                            for (OWLObjectProperty property : expression.getObjectPropertiesInSignature())
+                            {
+                                type = property.toStringID();
+                            }
+                            for (OWLClass associatedClass : expression.getClassesInSignature()){
+                                namePro = associatedClass.toStringID();
+                            }
+                            System.out.println("\t\t +: " + type + " "+namePro);
+                        }
+                        alternativeDefinitions.add(associatedTerms);
+                }
+            }
+            /*
+                for(OWLNamedIndividual i: reasoner.getInstances(c, true).getFlattened()){
+                    System.out.println(labelFor(i, ontology) +":"+ labelFor(c, ontology));
+                    
+                    
+                    for (OWLObjectProperty op: ontology.getObjectPropertiesInSignature()) {
+                        NodeSet<OWLNamedIndividual> petValuesNodeSet = reasoner.getObjectPropertyValues(i, op);
+                        for (OWLNamedIndividual value : petValuesNodeSet.getFlattened())
+                            System.out.println(labelFor(i, ontology) + " " + 
+                                    labelFor(op, ontology) + " " + labelFor(value, ontology));
+                    }
+                }
+            }*/
+            
+            
             System.out.println("Classes");
             System.out.println("--------------------------------");
             for (OWLClass cls : classes) {
@@ -85,25 +190,78 @@ public class OwlapiHelp {
                         subClassList.add(owl_class.getIRI().getShortForm());
                     }
                 }
-                key2values.put("SUBCLASS", subClassList);
+                key2values.put("SUPERCLASS", subClassList);
                 
                 
                 System.out.println(" \tObject Property");
                 Iterator<OWLObjectProperty> objProIterator =cls.objectPropertiesInSignature().iterator();
                 while(objProIterator.hasNext()){
                     OWLObjectProperty objPro = objProIterator.next();
-                    System.out.println("\t\t +: " + objPro.getIRI().getShortForm());
+                    String z = "\t\t +: " + objPro.getIRI().getShortForm();
                 }
                 
                 
                 ArrayList<String> objectPropertyDomainList = new ArrayList<String>();
                 System.out.println(" \tObject Property Domain");
                 for (OWLObjectPropertyDomainAxiom op : ontology.getAxioms(AxiomType.OBJECT_PROPERTY_DOMAIN)) {
-                    if (op.getDomain().equals(cls)) {
+                    if (op.getDomain().equals(cls)) {                     
+                        
+                        AxiomType axiomType = op.getAxiomType();
+                        OWLAxiom axiom = op.getNNF();
+                        OWLObjectPropertyExpression o = op.getProperty();
+                        OWLObjectPropertyExpression oooo = o.getSimplified();
+                        OWLDatatype owlDatatypeeee = dataFactory.getOWLDatatype(o.getNamedProperty().getIRI());
+                        /*for (OWLClass associatedClass : op.getClassesInSignature())
+                        {
+                            System.out.println("\t\t +: " + associatedClass.getIRI().getShortForm());
+                        }
+                        for (OWLClassExpression associatedClass : op.getNestedClassExpressions())
+                        {
+                            System.out.println("\t\t +: " + associatedClass.getClassExpressionType() . getIRI().getShortForm());
+                            OWLClassExpression express = associatedClass.getObjectComplementOf();
+                            System.out.println("\t\t +: " +  express.toString());
+                            System.out.println("\t\t +: " + associatedClass.getNNF().toString());
+                        }
+                        for (OWLDataProperty associatedClass : op.getDataPropertiesInSignature())
+                        {
+                            System.out.println("\t\t +: " + associatedClass.getIRI().getShortForm());
+                        }
+                        for (OWLNamedIndividual associatedClass : op.getIndividualsInSignature())
+                        {
+                            System.out.println("\t\t +: " + associatedClass.getIRI().getShortForm());
+                        }
+                        for (OWLAnnotation associatedClass : op.getAnnotations())
+                        {
+                            System.out.println("\t\t +: " + associatedClass.getProperty().toStringID() + associatedClass.getValue().toString());
+                        }*/
+                        
+                        
+                        
+                        
+                        
+                        
+                        
                         for (OWLObjectProperty oop : op.getObjectPropertiesInSignature()) {
+                            
+                            /*Stream<OWLDatatype> owlDatatypeStream = oop.datatypesInSignature();
+                            Iterator<OWLDatatype> owlDatatypeIterator = owlDatatypeStream.iterator();
+                            while(owlDatatypeIterator.hasNext()){
+                                OWLDatatype oWLDatatype = owlDatatypeIterator.next();
+                                System.out.println("\t\t +: " + oWLDatatype.toStringID() + oWLDatatype.toString());
+                            }*/
+                            
+                            OWLDatatype owlDatatype = dataFactory.getOWLDatatype(oop.getIRI());
+                            //System.out.println(owlDatatype.getIRI());
+                            
                             System.out.println("\t\t +: " + oop.getIRI().getShortForm());
                             result += "\t\t +: " + oop.getIRI().getShortForm();
                             objectPropertyDomainList.add(oop.getIRI().getShortForm());
+                            
+                            
+                            
+                            OWLObjectPropertyImpl imp = (OWLObjectPropertyImpl) oop;
+                            EntityType eee = imp.getEntityType();
+                            String a = eee.getIRI().getShortForm();
                         }
                         //System.out.println("\t\t +: " + op.getProperty().getNamedProperty().getIRI().getShortForm());
                     }
@@ -115,10 +273,19 @@ public class OwlapiHelp {
                 System.out.println(" \tData Property Domain");
                 for (OWLDataPropertyDomainAxiom dp : ontology.getAxioms(AxiomType.DATA_PROPERTY_DOMAIN)) {
                     if (dp.getDomain().equals(cls)) {
+                        
                         for (OWLDataProperty odp : dp.getDataPropertiesInSignature()) {
                             System.out.println("\t\t +: " + odp.getIRI().getShortForm());
                             result += "\t\t +: " + odp.getIRI().getShortForm();
                             dataPropertyDomainList.add(odp.getIRI().getShortForm());
+                            
+                            
+                            /*String breakPoint = "";
+                            if(odp.getIRI().getShortForm().equals("isArray"))
+                                breakPoint = "TREY";
+                            OWLDatatype owlDatatype = dataFactory.getOWLDatatype(odp.getIRI());
+                            System.out.println(owlDatatype.getIRI());
+                            System.out.println(owlDatatype.isBoolean());*/
                         }
                         //System.out.println("\t\t +:" + dp.getProperty());
                     }
@@ -128,9 +295,10 @@ public class OwlapiHelp {
                 
                 ArrayList<String> annotationoPropertyDomainList = new ArrayList<String>();                
                 System.out.println(" \tAnnotation Property Domain");
-                for (OWLAnnotationPropertyDomainAxiom dp : ontology.getAxioms(AxiomType.ANNOTATION_PROPERTY_DOMAIN)) {
-                    if (dp.getDomain().equals(cls)) {
-                        for (OWLDataProperty odp : dp.getDataPropertiesInSignature()) {
+                for (OWLAnnotationPropertyDomainAxiom ap : ontology.getAxioms(AxiomType.ANNOTATION_PROPERTY_DOMAIN)) {
+                    if (ap.getDomain().equals(cls)) {
+                        
+                        for (OWLDataProperty odp : ap.getDataPropertiesInSignature()) {
                             System.out.println("\t\t +: " + odp.getIRI().getShortForm());
                             result += "\t\t +: " + odp.getIRI().getShortForm();
                             annotationoPropertyDomainList.add(odp.getIRI().getShortForm());
@@ -156,8 +324,8 @@ public class OwlapiHelp {
             
             
             
-            OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
-            OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
+            /*OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+            //OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
             for (OWLNamedIndividual i : ontology.getIndividualsInSignature()) {
                 for (OWLObjectProperty p : ontology.getObjectPropertiesInSignature()) {
                     NodeSet<OWLNamedIndividual> individualValues = reasoner.getObjectPropertyValues(i, p);
@@ -167,11 +335,13 @@ public class OwlapiHelp {
                         System.out.println(" " + ind);
                     }
                 }
-            }
+            }*/
 
         } catch (OWLOntologyCreationException ex) {
             log.error(ex);
         }
         return map;
     }
+    
+   
 }
