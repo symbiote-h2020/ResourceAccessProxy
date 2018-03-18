841a225b6f2c57e9e5d224a222133dbc151f589c
MarioKusek <mario.kusek@fer.hr>
2018-03-18 12:18:28 +0100

    maven publishing fat jar, spring boot to 1.5.10, wait for other services

--- src/main/java/eu/h2020/symbiote/ResourceAccessProxyApplication.java
+++ src/main/java/eu/h2020/symbiote/ResourceAccessProxyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResourceAccessProxyApplication {
     public static void main(String[] args) {
+    		WaitForPort.waitForServices(WaitForPort.findProperty("SPRING_BOOT_WAIT_FOR_SERVICES"));
         SpringApplication.run(ResourceAccessProxyApplication.class, args);
     }   
 }
